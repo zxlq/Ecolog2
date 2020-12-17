ce680248c4168a63838d0478d13043289173b81a
Joe Dolan <josephpdolan@gmail.com>
2020-12-17 16:22:03 +0000

    logo

--- src/DashboardCards/EcoHomePage.js
+++ src/DashboardCards/EcoHomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import cx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
- import Card from "@material-ui/core/Card";
+import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import TextInfoContent from "@mui-treasury/components/content/textInfo";
@@ -31,15 +31,12 @@ export const EcoHomePage = React.memo(function MusicCard() {
     <Card className={cx(styles.root, shadowStyles.root)}>
       <CardMedia
         className={cx(styles.media, mediaStyles.root)}
-        image={"https://images.unsplash.com/photo-1599940824399-b87987ceb72a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHRyZWVzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"}
-        
+        image={
+          "https://images.unsplash.com/photo-1563697092941-f17038b46152?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1713&q=80"
+        }
       />
       <CardContent>
-        <TextInfoContent
-          classes={textCardContentStyles}
-          overline={"EcoLog"}
-          
-        />
+        <TextInfoContent classes={textCardContentStyles} overline={"EcoLog"} />
       </CardContent>
     </Card>
   );
