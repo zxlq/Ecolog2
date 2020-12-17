0eb8cc3cfd9911b66fe4c312aee09ec0b23f12df
Joe Dolan <josephpdolan@gmail.com>
2020-12-17 16:31:23 +0000

    aa

--- src/DashboardCards/BicycleJourneys.js
+++ src/DashboardCards/BicycleJourneys.js
@@ -38,7 +38,7 @@ class BicycleJourneys extends Component {
           src="bicycle-svgrepo-com.svg"
           alt="Logo"
           width="250"
-          height="160"
+          height="170"
           align="center"
         ></img>
         <Typography component="p" variant="h3">
